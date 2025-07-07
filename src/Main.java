1f4ca2ea000bd922ca05c59c93572275e54e8e12
Shvets Sergey SDS <Sergey.Shvets@smartds.ru>
2025-07-07 19:21:58 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,98 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        String[] names = {"Artem", "Andrey", "Anton"};
+        System.out.println(names);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+
+        }
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println(names.length);
+        System.out.println(Arrays.stream(names).toList());
+        System.out.println(average(10));
+
+        int[][] matrix = new int[10][10];
+        int[][] grid = {{1, 2}, {3, 4}};
+        print(matrix);
+        print(grid);
+        printIndex(grid);
+        printIndex(matrix);
+
+        int[][] data = createMatrix(5);
+        print(data);
+        sum(data);
+    }
+
+    //O(2n) -> O(n)
+    /*
+    static int average(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = RANDOM.nextInt(100);
+        }
+        int sum = 0;
+        for (int e : arr) {
+            sum += e;
+        }
+        return sum / size;
+    }
+    */
+    static int average(int size) {
+        int[] arr = new int[size];
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            arr[i] = RANDOM.nextInt(100);
+            sum += arr[i];
+        }
+        return sum / size;
+    }
+
+    static void print(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void printIndex(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("(%s, %s) ", i, j);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static int[][] createMatrix(int size) {
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = RANDOM.nextInt(100);
+            }
+        }
+        return matrix;
+    }
+
+    static void sum(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+            }
+            System.out.printf("Строка %s: сумма = %s\n", i, sum);
+        }
+    }
+
+}
